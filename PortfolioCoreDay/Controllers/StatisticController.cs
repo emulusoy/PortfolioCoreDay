afee136eb9b23811d5f6595be2f9c7ab6818d468
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-15 15:57:12 +0300

    lesson 5

---
+++ PortfolioCoreDay/Controllers/StatisticController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioCoreDay.Context;
+
+namespace PortfolioCoreDay.Controllers
+{
+    public class StatisticController : Controller
+    {
+        PortfolioContext context=new PortfolioContext();
+        public IActionResult Index()
+        {
+            ViewBag.v1=context.Skills.Count();  
+            ViewBag.v2=context.Skills.Sum(x=>x.SkillValue);  
+            ViewBag.v3=context.Skills.Average(x => x.SkillValue);  
+            ViewBag.v4=context.Skills.Where(x => x.SkillValue>80).Count();  
+
+
+
+            return View();
+        }
+    }
+}
--- PortfolioCoreDay/obj/Debug/net8.0/PortfolioCoreDay.AssemblyInfo.cs
+++ PortfolioCoreDay/obj/Debug/net8.0/PortfolioCoreDay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioCoreDay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055a76d86981b263e04b211299a3c4712fc522b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d9bd9669b31de29fb5e785f618c4628bb69580")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioCoreDay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioCoreDay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

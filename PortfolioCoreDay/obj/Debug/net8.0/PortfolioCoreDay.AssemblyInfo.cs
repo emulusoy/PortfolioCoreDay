e360c7c29d94107b9712280ea027e663f47eb205
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-22 15:58:19 +0300

    Finished lesson 7

--- PortfolioCoreDay/obj/Debug/net8.0/PortfolioCoreDay.AssemblyInfo.cs
+++ PortfolioCoreDay/obj/Debug/net8.0/PortfolioCoreDay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioCoreDay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afee136eb9b23811d5f6595be2f9c7ab6818d468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406cbcc35328850e175819c30bc1eb20c2b58190")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioCoreDay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioCoreDay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
